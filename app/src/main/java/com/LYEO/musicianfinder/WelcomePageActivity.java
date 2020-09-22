6beeef36b69b8d3cf27b086e1c6a3f2bcff8e1b6
Lidor Eliyahu Shelef <ivanlidor98@gmail.com>
2020-09-22 17:52:44 +0300

    Created the basic page template of:  - Menu Page  - Private Area Page  - Band Search Page  - Musician Search Page

--- app/src/main/java/com/LYEO/musicianfinder/WelcomePageActivity.java
+++ app/src/main/java/com/LYEO/musicianfinder/WelcomePageActivity.java
@@ -1,18 +1,16 @@
 package com.LYEO.musicianfinder;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomePageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcome_page);
 
     }
     public  void mainClick (View v){
