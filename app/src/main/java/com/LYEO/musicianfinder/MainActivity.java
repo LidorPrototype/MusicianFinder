bc6c03ca991a4ae483bd966c46c28e18ecaf67e1
yisrael bar <yisrael35@gmail.com>
2020-09-22 17:04:29 +0300

    update

--- app/src/main/java/com/LYEO/musicianfinder/MainActivity.java
+++ app/src/main/java/com/LYEO/musicianfinder/MainActivity.java
@@ -2,7 +2,10 @@ package com.LYEO.musicianfinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public  void mainClick (View v){
+        if (v.getId()==R.id.btLogin){
+            Intent intent= new Intent(getApplicationContext(),Login.class);
+            startActivity(intent);
+        }
+        else if (v.getId()==R.id.btRegister){
+            Intent intent= new Intent(getApplicationContext(),Register.class);
+            startActivity(intent);
+
+        }
 
     }
 }
