889e98a4600bd1abd6d71810844d98780e513175
elie <ezezer2@hotmail.com>
2020-09-24 01:43:04 +0300

    update

--- app/src/main/java/com/LYEO/musicianfinder/MenuActivity.java
+++ app/src/main/java/com/LYEO/musicianfinder/MenuActivity.java
@@ -1,14 +1,71 @@
 package com.LYEO.musicianfinder;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MenuActivity extends AppCompatActivity {
-
+    private Button ToPrivateArea;
+    private Button ToFindABand;
+    private Button ToFindAMusician;
+    private Button ToPublishYouself;
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        ToPrivateArea=(Button) findViewById(R.id.privateArea);
+        ToFindABand=(Button) findViewById(R.id.FindABand);
+        ToFindAMusician=(Button) findViewById(R.id.FindAMusician);
+        ToPublishYouself=(Button) findViewById(R.id.PublishYourself);
+
+        ToPrivateArea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPrivateArea();
+            }
+        });
+        ToFindABand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFindABand();
+            }
+        });
+        ToFindAMusician.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFindAMusician();
+            }
+        });
+        ToPublishYouself.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPublishYouself();
+            }
+        });
+
+
+
+
+        }
+        public void openPrivateArea(){
+        intent=new Intent(this,PrivateAreaActivity.class);
+        startActivity(intent);
     }
-}
+        public void openFindABand(){
+        intent=new Intent(this,BandSearchScreenActivity.class);
+        startActivity(intent);
+    }
+        public void openFindAMusician(){
+        intent=new Intent(this,MusicianSearchScreenActivity.class);
+        startActivity(intent);
+    }
+        public void openPublishYouself(){
+        intent=new Intent(this,PublishYourselfActivity.class);
+        startActivity(intent);
+    }
+    }
